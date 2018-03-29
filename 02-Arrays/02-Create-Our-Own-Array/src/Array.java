73defc55a7193985c3848edd6a2498fd9c2aff6d
liuyubobobo <liuyubobobo@gmail.com>
2018-03-29 00:34:28 -0700

    Chapter 02 section 02 completed.

---
+++ 02-Arrays/02-Create-Our-Own-Array/src/Array.java
@@ -0,0 +1,31 @@
+public class Array {
+
+    private int[] data;
+    private int size;
+
+    // 构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = new int[capacity];
+        size = 0;
+    }
+
+    // 无参数的构造函数，默认数组的容量capacity=10
+    public Array(){
+        this(10);
+    }
+
+    // 获取数组的容量
+    public int getCapacity(){
+        return data.length;
+    }
+
+    // 获取数组中的元素个数
+    public int getSize(){
+        return size;
+    }
+
+    // 返回数组是否为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+}

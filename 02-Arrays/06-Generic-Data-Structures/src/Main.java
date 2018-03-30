9e34baeab635914d7f39c63fafdf31a8704509f9
liuyubobobo <liuyubobobo@gmail.com>
2018-03-30 16:41:08 -0700

    Chapter 02 section 06 completed.

---
+++ 02-Arrays/06-Generic-Data-Structures/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Array<Integer> arr = new Array<>(20);
+        for(int i = 0 ; i < 10 ; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+        // [-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+    }
+}
---
+++ 02-Arrays/06-Generic-Data-Structures/src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+    private String name;
+    private int score;
+
+    public Student(String studentName, int studentScore){
+        name = studentName;
+        score = studentScore;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Student(name: %s, score: %d)", name, score);
+    }
+
+    public static void main(String[] args) {
+
+        Array<Student> arr = new Array<>();
+        arr.addLast(new Student("Alice", 100));
+        arr.addLast(new Student("Bob", 66));
+        arr.addLast(new Student("Charlie", 88));
+        System.out.println(arr);
+    }
+}

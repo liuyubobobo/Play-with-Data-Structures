e10fa347e2a90ed482490b82361cbc6e582b5b75
liuyubobobo <liuyubobobo@gmail.com>
2018-03-31 14:59:10 -0700

    Chapter 02 section 07 completed.

---
+++ 02-Arrays/07-Dynamic-Array/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Array<Integer> arr = new Array<>();
+        for(int i = 0 ; i < 10 ; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+    }
+}

60e9fb6d979c11b2237294f674fed97db85ca496
liuyubobobo <liuyubobobo@gmail.com>
2018-04-02 23:56:27 -0700

    Chapter 03 section 02 completed.

---
+++ 03-Stacks-and-Queues/02-Array-Stack/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayStack<Integer> stack = new ArrayStack<>();
+
+        for(int i = 0 ; i < 5 ; i ++){
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+    }
+}

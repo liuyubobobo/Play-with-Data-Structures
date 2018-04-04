1a807f81a2fccbc4094703f804f8b17f75bdc617
liuyubobobo <liuyubobobo@gmail.com>
2018-04-04 15:35:45 -0700

    Chapter 03 section 07 completed.

---
+++ 03-Stacks-and-Queues/07-Implementation-of-Loop-Queue/src/Array.java
@@ -0,0 +1,156 @@
+
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    // 构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = (E[])new Object[capacity];
+        size = 0;
+    }
+
+    // 无参数的构造函数，默认数组的容量capacity=10
+    public Array(){
+        this(10);
+    }
+
+    // 获取数组的容量
+    public int getCapacity(){
+        return data.length;
+    }
+
+    // 获取数组中的元素个数
+    public int getSize(){
+        return size;
+    }
+
+    // 返回数组是否为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // 在index索引的位置插入一个新元素e
+    public void add(int index, E e){
+
+        if(index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+
+        if(size == data.length)
+            resize(2 * data.length);
+
+        for(int i = size - 1; i >= index ; i --)
+            data[i + 1] = data[i];
+
+        data[index] = e;
+
+        size ++;
+    }
+
+    // 向所有元素后添加一个新元素
+    public void addLast(E e){
+        add(size, e);
+    }
+
+    // 在所有元素前添加一个新元素
+    public void addFirst(E e){
+        add(0, e);
+    }
+
+    // 获取index索引位置的元素
+    public E get(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        return data[index];
+    }
+
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    public E getFirst(){
+        return get(0);
+    }
+
+    // 修改index索引位置的元素为e
+    public void set(int index, E e){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Index is illegal.");
+        data[index] = e;
+    }
+
+    // 查找数组中是否有元素e
+    public boolean contains(E e){
+        for(int i = 0 ; i < size ; i ++){
+            if(data[i].equals(e))
+                return true;
+        }
+        return false;
+    }
+
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(E e){
+        for(int i = 0 ; i < size ; i ++){
+            if(data[i].equals(e))
+                return i;
+        }
+        return -1;
+    }
+
+    // 从数组中删除index位置的元素, 返回删除的元素
+    public E remove(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+
+        E ret = data[index];
+        for(int i = index + 1 ; i < size ; i ++)
+            data[i - 1] = data[i];
+        size --;
+        data[size] = null; // loitering objects != memory leak
+
+        if(size == data.length / 4 && data.length / 2 != 0)
+            resize(data.length / 2);
+        return ret;
+    }
+
+    // 从数组中删除第一个元素, 返回删除的元素
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    // 从数组中删除最后一个元素, 返回删除的元素
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    // 从数组中删除元素e
+    public void removeElement(E e){
+        int index = find(e);
+        if(index != -1)
+            remove(index);
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for(int i = 0 ; i < size ; i ++){
+            res.append(data[i]);
+            if(i != size - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    // 将数组空间的容量变成newCapacity大小
+    private void resize(int newCapacity){
+
+        E[] newData = (E[])new Object[newCapacity];
+        for(int i = 0 ; i < size ; i ++)
+            newData[i] = data[i];
+        data = newData;
+    }
+}
---
+++ 03-Stacks-and-Queues/07-Implementation-of-Loop-Queue/src/LoopQueue.java
@@ -0,0 +1,106 @@
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    private int size;  // 有兴趣的同学，在完成这一章后，可以思考一下：
+                       // LoopQueue中不声明size，如何完成所有的逻辑？
+                       // 这个问题可能会比大家想象的要难一点点：）
+
+    public LoopQueue(int capacity){
+        data = (E[])new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue(){
+        this(10);
+    }
+
+    public int getCapacity(){
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return front == tail;
+    }
+
+    @Override
+    public int getSize(){
+        return size;
+    }
+
+    @Override
+    public void enqueue(E e){
+
+        if((tail + 1) % data.length == front)
+            resize(getCapacity() * 2);
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
+    }
+
+    @Override
+    public E dequeue(){
+
+        if(isEmpty())
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size --;
+        if(size == getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
+        return ret;
+    }
+
+    @Override
+    public E getFront(){
+        if(isEmpty())
+            throw new IllegalArgumentException("Queue is empty.");
+        return data[front];
+    }
+
+    private void resize(int newCapacity){
+
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for(int i = 0 ; i < size ; i ++)
+            newData[i] = data[(i + front) % data.length];
+
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: size = %d , capacity = %d\n", size, getCapacity()));
+        res.append("front [");
+        for(int i = front ; i != tail ; i = (i + 1) % data.length){
+            res.append(data[i]);
+            if((i + 1) % data.length != tail)
+                res.append(", ");
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+    public static void main(String[] args){
+
+        LoopQueue<Integer> queue = new LoopQueue<>();
+        for(int i = 0 ; i < 10 ; i ++){
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if(i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}

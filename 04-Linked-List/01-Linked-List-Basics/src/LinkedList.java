4760c41610bcbc68f72e0baaaf2e30e5f5b6e322
liuyubobobo <liuyubobobo@gmail.com>
2018-04-05 13:18:39 -0700

    Chapter 04 section 01 completed.

---
+++ 04-Linked-List/01-Linked-List-Basics/src/LinkedList.java
@@ -0,0 +1,26 @@
+public class LinkedList<E> {
+
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e, null);
+        }
+
+        public Node(){
+            this(null, null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+    }
+
+}

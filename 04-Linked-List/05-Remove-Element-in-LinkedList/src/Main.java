24d71ce43506ba39622254d8a862116f6ca1fd3d
liuyubobobo <liuyubobobo@gmail.com>
2018-04-05 20:02:38 -0700

    Chapter 04 section 05 completed.

---
+++ 04-Linked-List/05-Remove-Element-in-LinkedList/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i = 0 ; i < 5 ; i ++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+}

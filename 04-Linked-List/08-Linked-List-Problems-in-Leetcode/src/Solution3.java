3b8a2e4b9c6c7102aa4749af9088c60562742fa5
liuyubobobo <liuyubobobo@gmail.com>
2018-04-06 17:38:16 -0700

    Chapter 04 section 08 completed.

---
+++ 04-Linked-List/08-Linked-List-Problems-in-Leetcode/src/Solution3.java
@@ -0,0 +1,31 @@
+/// Leetcode 203. Remove Linked List Elements
+/// https://leetcode.com/problems/remove-linked-list-elements/description/
+
+class Solution3 {
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while(prev.next != null){
+            if(prev.next.val == val)
+                prev.next = prev.next.next;
+            else
+                prev = prev.next;
+        }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution3()).removeElements(head, 6);
+        System.out.println(res);
+    }
+}

309f2875b9bb61952fe20188f2b6b5b9569bf8ed
liuyubobobo <liuyubobobo@gmail.com>
2018-04-10 11:59:22 -0700

    Chapter 06 section 04 completed.

---
+++ 06-Binary-Search-Tree/04-Improved-Add-Elements-in-BST/src/BST.java
@@ -0,0 +1,50 @@
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST(){
+        root = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // 向二分搜索树中添加新的元素e
+    public void add(E e){
+        root = add(root, e);
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e){
+        if(node == null){
+            size ++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
+    }
+}
---
+++ 06-Binary-Search-Tree/04-Improved-Add-Elements-in-BST/src/Solution.java
@@ -0,0 +1,72 @@
+/// Leetcode 804. Unique Morse Code Words
+/// https://leetcode.com/problems/unique-morse-code-words/description/
+///
+/// 课程中在这里暂时没有介绍这个问题
+/// 该代码主要用于使用Leetcode上的问题测试我们的BST类
+public class Solution {
+
+    private class BST<E extends Comparable<E>> {
+
+        private class Node {
+            public E e;
+            public Node left, right;
+
+            public Node(E e) {
+                this.e = e;
+                left = null;
+                right = null;
+            }
+        }
+
+        private Node root;
+        private int size;
+
+        public BST(){
+            root = null;
+            size = 0;
+        }
+
+        public int size(){
+            return size;
+        }
+
+        public boolean isEmpty(){
+            return size == 0;
+        }
+
+        // 向二分搜索树中添加新的元素e
+        public void add(E e){
+            root = add(root, e);
+        }
+
+        // 向以node为根的二分搜索树中插入元素e，递归算法
+        // 返回插入新节点后二分搜索树的根
+        private Node add(Node node, E e){
+            if(node == null){
+                size ++;
+                return new Node(e);
+            }
+
+            if(e.compareTo(node.e) < 0)
+                node.left = add(node.left, e);
+            else if(e.compareTo(node.e) > 0)
+                node.right = add(node.right, e);
+
+            return node;
+        }
+    }
+
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        BST<String> bst = new BST<>();
+        for(String word: words){
+            StringBuilder res = new StringBuilder();
+            for(int i = 0 ; i < word.length() ; i ++)
+                res.append(codes[word.charAt(i) - 'a']);
+            bst.add(res.toString());
+        }
+
+        return bst.size();
+    }
+}

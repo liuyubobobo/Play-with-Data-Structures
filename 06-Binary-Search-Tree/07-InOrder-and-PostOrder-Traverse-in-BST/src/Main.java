3ce4d4abdeeba7606b675f9598fa51d11e82ddb7
liuyubobobo <liuyubobobo@gmail.com>
2018-04-12 02:27:30 -0700

    Chapter 06 section 07 completed.

---
+++ 06-Binary-Search-Tree/07-InOrder-and-PostOrder-Traverse-in-BST/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder();
+        System.out.println();
+
+        bst.postOrder();
+        System.out.println();
+    }
+}

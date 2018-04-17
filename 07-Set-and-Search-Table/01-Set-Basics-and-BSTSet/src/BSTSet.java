8b729348506a5ac6fad36ed3a413deecdbc5580e
liuyubobobo <liuyubobobo@gmail.com>
2018-04-16 18:22:57 -0700

    Chapter 07 section 01 completed.

---
+++ 07-Set-and-Search-Table/01-Set-Basics-and-BSTSet/src/BSTSet.java
@@ -0,0 +1,33 @@
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+
+    private BST<E> bst;
+
+    public BSTSet(){
+        bst = new BST<>();
+    }
+
+    @Override
+    public int getSize(){
+        return bst.size();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return bst.isEmpty();
+    }
+
+    @Override
+    public void add(E e){
+        bst.add(e);
+    }
+
+    @Override
+    public boolean contains(E e){
+        return bst.contains(e);
+    }
+
+    @Override
+    public void remove(E e){
+        bst.remove(e);
+    }
+}
---
+++ 07-Set-and-Search-Table/01-Set-Basics-and-BSTSet/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            BSTSet<String> set1 = new BSTSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
+        System.out.println();
+
+
+        System.out.println("A Tale of Two Cities");
+
+        ArrayList<String> words2 = new ArrayList<>();
+        if(FileOperation.readFile("a-tale-of-two-cities.txt", words2)){
+            System.out.println("Total words: " + words2.size());
+
+            BSTSet<String> set2 = new BSTSet<>();
+            for(String word: words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
+    }
+}

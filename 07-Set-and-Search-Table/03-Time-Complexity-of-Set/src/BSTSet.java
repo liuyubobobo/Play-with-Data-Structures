691e7fc6dd0394ea627f66a44dcf33f5bee0dfaf
liuyubobobo <liuyubobobo@gmail.com>
2018-04-16 23:09:04 -0700

    Chapter 07 section 03 completed.

---
+++ 07-Set-and-Search-Table/03-Time-Complexity-of-Set/src/BSTSet.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+
+    private BST<E> bst;
+
+    public BSTSet(){
+        bst = new BST<>();
+    }
+
+    @Override
+    public int getSize(){
+        return bst.size();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return bst.isEmpty();
+    }
+
+    @Override
+    public void add(E e){
+        bst.add(e);
+    }
+
+    @Override
+    public boolean contains(E e){
+        return bst.contains(e);
+    }
+
+    @Override
+    public void remove(E e){
+        bst.remove(e);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            BSTSet<String> set1 = new BSTSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
+        System.out.println();
+
+
+        System.out.println("A Tale of Two Cities");
+
+        ArrayList<String> words2 = new ArrayList<>();
+        if(FileOperation.readFile("a-tale-of-two-cities.txt", words2)){
+            System.out.println("Total words: " + words2.size());
+
+            BSTSet<String> set2 = new BSTSet<>();
+            for(String word: words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
+    }
+}
---
+++ 07-Set-and-Search-Table/03-Time-Complexity-of-Set/src/LinkedListSet.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class LinkedListSet<E> implements Set<E> {
+
+    private LinkedList<E> list;
+
+    public LinkedListSet(){
+        list = new LinkedList<>();
+    }
+
+    @Override
+    public int getSize(){
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    @Override
+    public void add(E e){
+        if(!list.contains(e))
+            list.addFirst(e);
+    }
+
+    @Override
+    public boolean contains(E e){
+        return list.contains(e);
+    }
+
+    @Override
+    public void remove(E e){
+        list.removeElement(e);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            LinkedListSet<String> set1 = new LinkedListSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
+        System.out.println();
+
+
+        System.out.println("A Tale of Two Cities");
+
+        ArrayList<String> words2 = new ArrayList<>();
+        if(FileOperation.readFile("a-tale-of-two-cities.txt", words2)) {
+            System.out.println("Total words: " + words2.size());
+
+            LinkedListSet<String> set2 = new LinkedListSet<>();
+            for (String word : words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
+    }
+}

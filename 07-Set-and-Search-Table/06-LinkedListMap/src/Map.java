589d123538480435262c96dd2c9bfbcebc81d1a5
liuyubobobo <liuyubobobo@gmail.com>
2018-04-17 12:10:11 -0700

    Chapter 07 section 06 completed.

---
+++ 07-Set-and-Search-Table/06-LinkedListMap/src/Map.java
@@ -0,0 +1,10 @@
+public interface Map<K, V> {
+
+    void add(K key, V value);
+    V remove(K key);
+    boolean contains(K key);
+    V get(K key);
+    void set(K key, V newValue);
+    int getSize();
+    boolean isEmpty();
+}

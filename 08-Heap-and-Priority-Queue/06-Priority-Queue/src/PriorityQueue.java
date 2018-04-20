f54a77bb30d13cc0417e4b60c123464eaf57aaa4
liuyubobobo <liuyubobobo@gmail.com>
2018-04-19 21:15:16 -0700

    Chapter 08 section 06 completed.

---
+++ 08-Heap-and-Priority-Queue/06-Priority-Queue/src/PriorityQueue.java
@@ -0,0 +1,33 @@
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue(){
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize(){
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public E getFront(){
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public void enqueue(E e){
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue(){
+        return maxHeap.extractMax();
+    }
+}

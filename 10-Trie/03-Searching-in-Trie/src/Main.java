28e7507354140306a2f1ddb0d65ce1f847ae28fd
liuyubobobo <liuyubobobo@gmail.com>
2018-04-26 18:27:50 -0700

    Chapter 10 section 03 completed.

---
+++ 10-Trie/03-Searching-in-Trie/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words)){
+
+            long startTime = System.nanoTime();
+
+            BSTSet<String> set = new BSTSet<>();
+            for(String word: words)
+                set.add(word);
+
+            for(String word: words)
+                set.contains(word);
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + set.getSize());
+            System.out.println("BSTSet: " + time + " s");
+
+            // ---
+
+            startTime = System.nanoTime();
+
+            Trie trie = new Trie();
+            for(String word: words)
+                trie.add(word);
+
+            for(String word: words)
+                trie.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + trie.getSize());
+            System.out.println("Trie: " + time + " s");
+        }
+    }
+}
---
+++ 10-Trie/03-Searching-in-Trie/src/Set.java
@@ -0,0 +1,8 @@
+public interface Set<E> {
+
+    void add(E e);
+    boolean contains(E e);
+    void remove(E e);
+    int getSize();
+    boolean isEmpty();
+}

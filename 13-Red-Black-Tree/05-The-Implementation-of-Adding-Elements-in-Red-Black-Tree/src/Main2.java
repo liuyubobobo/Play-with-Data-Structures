4c482e209992482d51369f8f2080cfb268de2e05
liuyubobobo <liuyubobobo@gmail.com>
2018-05-14 00:26:19 +0800

    Chapter 13 codes added.

---
+++ 13-Red-Black-Tree/05-The-Implementation-of-Adding-Elements-in-Red-Black-Tree/src/Main2.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        int n = 20000000;
+
+        Random random = new Random(n);
+        ArrayList<Integer> testData = new ArrayList<>(n);
+        for(int i = 0 ; i < n ; i ++)
+            testData.add(random.nextInt(Integer.MAX_VALUE));
+
+        // Test BST
+        long startTime = System.nanoTime();
+
+        BST<Integer, Integer> bst = new BST<>();
+        for (Integer x: testData)
+            bst.add(x, null);
+
+        long endTime = System.nanoTime();
+
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println("BST: " + time + " s");
+
+
+        // Test AVL
+        startTime = System.nanoTime();
+
+        AVLTree<Integer, Integer> avl = new AVLTree<>();
+        for (Integer x: testData)
+            avl.add(x, null);
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("AVL: " + time + " s");
+
+
+        // Test RBTree
+        startTime = System.nanoTime();
+
+        RBTree<Integer, Integer> rbt = new RBTree<>();
+        for (Integer x: testData)
+            rbt.add(x, null);
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("RBTree: " + time + " s");
+    }
+}

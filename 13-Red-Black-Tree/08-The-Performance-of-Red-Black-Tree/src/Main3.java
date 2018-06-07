5e5425a364afffe33ac2a4291ed22ac87167aa24
liuyubobobo <liuyubobobo@gmail.com>
2018-06-07 12:39:59 -0700

    Chapter 13 completed.

--- 13-Red-Black-Tree/08-The-Performance-of-Red-Black-Tree/src/Main3.java
+++ 13-Red-Black-Tree/08-The-Performance-of-Red-Black-Tree/src/Main3.java
@@ -1,41 +1,26 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Main2 {
+public class Main3 {
 
     public static void main(String[] args) {
 
-        // int n = 20000000;
         int n = 20000000;
 
-        Random random = new Random(n);
         ArrayList<Integer> testData = new ArrayList<>(n);
         for(int i = 0 ; i < n ; i ++)
-            testData.add(random.nextInt(Integer.MAX_VALUE));
-
-        // Test BST
-        long startTime = System.nanoTime();
-
-        BST<Integer, Integer> bst = new BST<>();
-        for (Integer x: testData)
-            bst.add(x, null);
-
-        long endTime = System.nanoTime();
-
-        double time = (endTime - startTime) / 1000000000.0;
-        System.out.println("BST: " + time + " s");
-
+            testData.add(i);
 
         // Test AVL
-        startTime = System.nanoTime();
+        long startTime = System.nanoTime();
 
         AVLTree<Integer, Integer> avl = new AVLTree<>();
         for (Integer x: testData)
             avl.add(x, null);
 
-        endTime = System.nanoTime();
+        long endTime = System.nanoTime();
 
-        time = (endTime - startTime) / 1000000000.0;
+        double time = (endTime - startTime) / 1000000000.0;
         System.out.println("AVL: " + time + " s");
 
 
